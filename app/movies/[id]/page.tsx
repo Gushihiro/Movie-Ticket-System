c09acbfe29360e4d6ef9fa1d453233fde0143130
Hiroto Robinson <robinsonhiroto@gmail.com>
2024-03-09 17:15:02 -0800

    Adjusting individual movie page to be styled properly

--- app/movies/[id]/page.tsx
+++ app/movies/[id]/page.tsx
@@ -6,11 +6,18 @@ import Link from "next/link";
 export default async function Page({ params }: { params: { id: string } }) {
   const id = params.id;
   const selectedMovie: Movie = await getMovieById(id);
-  console.log(selectedMovie);
+  console.log(selectedMovie, "Selected Movie");
   return (
-    <main>
-      <Image src={`https://image.tmdb.org/t/p/w500${selectedMovie.poster_path}`} alt={selectedMovie.overview} width={180} height={300} />
-        {selectedMovie.title}
+    <main className={'flex flex-row items-center text-center'}>
+      <Image className={'px-4'} src={`https://image.tmdb.org/t/p/w500${selectedMovie.poster_path}`} alt={selectedMovie.overview} width={360} height={600} />
+      <div>
+        <div className={'text-5xl py-4'}>
+          {selectedMovie.title}
+        </div>
+        <div className={'max-w-3xl'}>
+          {selectedMovie.overview}
+        </div>
+      </div>
     </main>
   );
 }
