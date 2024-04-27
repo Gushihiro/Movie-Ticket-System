638520255d8d6e171acdddef2c6097455558ad19
Hiroto Robinson <robinsonhiroto@gmail.com>
2024-04-27 16:11:49 -0700

    Created a background and border radius for movie card. Added padding and slight opacity to navbar

--- app/ui/card/movie-card.tsx
+++ app/ui/card/movie-card.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 export default function MovieCard(movie: Movie, index: number) {
   return (
-    <div key={index}>
+    <div key={index} className={'bg-gray-700 h-80 rounded mx-4'}>
       <Link className='flex flex-col items-center text-center' href={`/movies/${movie.id}`}>
         <Image src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.overview} width={180} height={300} />
         {movie.title}
